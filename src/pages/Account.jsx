7150447e9322b34de42f2baaeee7ee4d3d04d947
Bhavesh Patil <145092023+iambhvsh@users.noreply.github.com>
2024-06-12 15:22:47 +0530

    Update Account.jsx

--- src/pages/Account.jsx
+++ src/pages/Account.jsx
@@ -7,6 +7,7 @@ import {
   List,
   ListItem,
   ListButton,
+  BlockTitle,
 } from 'framework7-react';
 
 import './Account.less';
@@ -26,27 +27,80 @@ const Account = () => {
             chevronCenter
             link
           >
-            <img slot="media" src="https://iambhvsh.vercel.app/assets/blog/authors/iambhvsh.png" alt="Bhavesh Patil" />
+            <img slot="media" src="iambhvsh.png" alt="Bhavesh Patil" />
           </ListItem>
         </List>
+        
+        <BlockTitle>User Profile</BlockTitle>
         <List>
           <ListItem
-            title="About"
-            link
+            title="Full Name"
+            after="Bhavesh Patil"
           />
           <ListItem
-            title="Source Code" link="https://github.com/iambhvsh/app-store"
+            title="Username"
+            after="@iambhvsh"
+          />
+          <ListItem
+            title="Email"
+            after="bhavesh@example.com"
           />
         </List>
+        
+        <BlockTitle>Account Actions</BlockTitle>
         <List>
           <ListButton>Redeem Gift Card</ListButton>
           <ListButton>Send Gift Card by Email</ListButton>
           <ListButton>Add Funds to Apple ID</ListButton>
         </List>
+
+        <BlockTitle>Information</BlockTitle>
+        <List>
+          <ListItem
+            title="About"
+            link="#"
+          />
+          <ListItem
+            title="Source Code"
+            link="https://github.com/iambhvsh/app-store"
+          />
+          <ListItem
+            title="Privacy Policy"
+            link="#"
+          />
+          <ListItem
+            title="Terms of Service"
+            link="#"
+          />
+        </List>
+
+        <BlockTitle>Settings</BlockTitle>
         <List>
           <ListItem
             title="Personalised Recommendations"
-            link
+            link="#"
+          />
+        </List>
+
+        <BlockTitle>Connect with Me</BlockTitle>
+        <List>
+          <ListItem
+            title="Twitter"
+            link="https://twitter.com/iambhvsh"
+            external
+            target="_blank"
+          />
+          <ListItem
+            title="LinkedIn"
+            link="https://linkedin.com/in/iambhvsh"
+            external
+            target="_blank"
+          />
+          <ListItem
+            title="GitHub"
+            link="https://github.com/iambhvsh"
+            external
+            target="_blank"
           />
         </List>
       </Page>
