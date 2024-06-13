a19ba26cbebe3073a0d1b76a2e027359b102a932
Bhavesh Patil <145092023+iambhvsh@users.noreply.github.com>
2024-06-13 09:37:12 +0530

    Update data.js

--- src/js/data.js
+++ src/js/data.js
@@ -1,20 +1,26 @@
-/* eslint-disable */
 const gamesData = require('../data/games.json');
 const appsData = require('../data/apps.json');
 
 const games = gamesData.map((game) => {
   if (game.icon) game.icon = `static/apps-images/${game.icon}`;
+
   game.screenshots = game.screenshots.map((fileName) => `static/apps-images/${fileName}`);
-  game.screenshotsThumbs = game.screenshots.map((fileName) => fileName.replace('.jpg', '-thumb.jpg'));
+
+  // Directly use the thumbnail field if it exists
+  if (game.thumbnail) game.thumbnail = `static/apps-images/${game.thumbnail}`;
+
   return game;
 });
 
 const apps = appsData.map((app) => {
   if (app.icon) app.icon = `static/apps-images/${app.icon}`;
+
   app.screenshots = app.screenshots.map((fileName) => `static/apps-images/${fileName}`);
-  app.screenshotsThumbs = app.screenshots.map((fileName) => fileName.replace('.jpg', '-thumb.jpg'));
+
+  // Directly use the thumbnail field if it exists
+  if (app.thumbnail) app.thumbnail = `static/apps-images/${app.thumbnail}`;
+
   return app;
 });
 
 export { games, apps };
-
