0c911378d3c9f545b7c1dceedd04a2b96661095b
Bhavesh Patil <145092023+iambhvsh@users.noreply.github.com>
2024-06-14 08:39:01 +0530

    Update Today.jsx

--- src/pages/Today.jsx
+++ src/pages/Today.jsx
@@ -53,7 +53,6 @@ const Today = () => {
           image="https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcQark5NoC2-pK418pUOKf_eV2jg0U8M0Wsby3XZONawJXKS0y4PLRc_yXfo&s=10"
           title={(<span>APP<br />OF THE<br />DAY</span>)}
           titleColor="#fff"
-          background="#fff"
           titlePosition="bottom"
           titleLarge
           app={appYoutube}
