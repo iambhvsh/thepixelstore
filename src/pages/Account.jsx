8e4a2cb24a1c6fb9adb3a0863c59c37a2bf913f1
Bhavesh Patil <145092023+iambhvsh@users.noreply.github.com>
2024-06-17 17:04:52 +0530

    Update Account.jsx

--- src/pages/Account.jsx
+++ src/pages/Account.jsx
@@ -16,7 +16,7 @@ const Account = () => {
   return (
     <Popup push swipeToClose="to-bottom" className="account-popup">
       <Page className="account-page">
-        <Navbar title="Developer">
+        <Navbar title="Account">
           <Link popupClose slot="right">Done</Link>
         </Navbar>
         <List mediaList>
@@ -31,28 +31,24 @@ const Account = () => {
           </ListItem>
         </List>
         <List>
-          <ListItem
-            title="Github"
-            href="https://github.com/iambhvsh"
-          />
-          <ListItem
-            title="Telegram"
-            href="https://t.me/iambhvsh"
-          />
-          <ListItem
-            title="Website"
-            href="https://iambhvsh.vercel.app"
-          />
+          <ListItem title="Github">
+            <Link external href="https://github.com/iambhvsh">Github</Link>
+          </ListItem>
+          <ListItem title="Telegram">
+            <Link external href="https://t.me/iambhvsh">Telegram</Link>
+          </ListItem>
+          <ListItem title="Website">
+            <Link external href="https://iambhvsh.vercel.app">Website</Link>
+          </ListItem>
         </List>
         <List>
-          <ListButton href="mailto:iambhvshh@outlook.com">Apple ID</ListButton>
-          <ListButton href="mailto:iam.bhvsh@gmail.com">Google ID</ListButton>
+          <ListButton external href="mailto:iambhvshh@outlook.com">Apple ID</ListButton>
+          <ListButton external href="mailto:iam.bhvsh@gmail.com">Google ID</ListButton>
         </List>
         <List>
-          <ListItem
-            title="Install XAPK"
-            href="https://d.apkpure.com/b/APK/com.wuliang.xapkinstaller?version=latest"
-          />
+          <ListItem title="Install XAPK">
+            <Link external href="https://d.apkpure.com/b/APK/com.wuliang.xapkinstaller?version=latest">Install XAPK</Link>
+          </ListItem>
         </List>
       </Page>
     </Popup>
